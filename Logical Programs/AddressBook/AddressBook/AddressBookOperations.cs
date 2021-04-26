9fab27526e022a8854cdea69a8e324a3f4501ecd
Bhu087 <bhu087@gmail.com>
2021-04-26 16:24:32 +0530

    [Bhushan] Add. Address Book logic Added

--- Logical Programs/AddressBook/AddressBook/AddressBookOperations.cs
+++ Logical Programs/AddressBook/AddressBook/AddressBookOperations.cs
@@ -6,5 +6,20 @@ namespace AddressBook
 {
     class AddressBookOperations
     {
+        public void AddressOperationsSelcection()
+        {
+            Console.WriteLine("Enter Options");
+            Console.WriteLine("1 for Add Person");
+            Console.WriteLine("2 for Delete Person");
+            Console.WriteLine("3 for Edit Person");
+            try
+            {
+                int option = int.Parse(Console.ReadLine());
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
     }
 }
